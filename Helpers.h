d6e20592da46b2f4050a2ef2755d7d5b47980842
OlegVorontsov <oleg-voroncov@mail.ru>
2022-04-04 11:14:42 +0300

    Add project files.

---
+++ Helpers.h
@@ -0,0 +1,5 @@
+
+long long square(int a, int b)
+{
+	return (a + b) * (a + b);
+}
---
+++ Main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	std::cout << "HelloWorld!" << std::endl;
+
+	int result = square(2, 3);
+	std::cout << "Result = " << result << std::endl;
+}
